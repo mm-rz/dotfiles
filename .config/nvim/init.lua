40e39568cb97dcd24010b0bfae23a529378af726
mmrz(from ubuntu24) <univ.t1c2ha3@gmail.com>
2026-08-17 14:24:20 +0900

    space2に変更

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,21 +1,21 @@
 vim.loader.enable()
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
 vim.opt.rtp:prepend(lazypath)
 
 vim.opt.number = true
 
 require("lazy").setup("plugins", {})
-		
+    
 vim.opt.clipboard:append({"unnamedplus"})
 
 local function apply_cpp_highlight()
