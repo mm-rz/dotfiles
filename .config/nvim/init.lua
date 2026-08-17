5c4e748799c138b7197771207f4d3e40fef89b55
mmrz(from ubuntu24) <univ.t1c2ha3@gmail.com>
2026-08-17 14:22:49 +0900

    タブ幅4fix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -31,6 +31,7 @@ vim.api.nvim_create_autocmd("ColorScheme", {
 vim.api.nvim_create_autocmd("FileType", {
   pattern = "cpp",
   callback = function()
+    vim.opt_local.expandtab = true
     vim.opt_local.tabstop = 4
     vim.opt_local.shiftwidth = 4
     vim.opt_local.softtabstop = 4
